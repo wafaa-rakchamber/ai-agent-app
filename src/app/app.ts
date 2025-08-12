888a72c6c644ef9b5e697fa8ec8064eb22517568
WAFAA <wafaa@rakchamber.ae>
2025-08-12 11:12:12 +0400

    clear unused component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TodoAppComponent } from './todo/todo-app.component';
-import { WafaaProject } from './wafaa-project/wafaa-project';
-
-import { CounterComponent } from './counter/counter.component';
-
-import { AamnaProject } from './aamna-project/aamna-project';
-
-import { Counter1 } from './counter1/counter1';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TodoAppComponent, WafaaProject, Counter1,AamnaProject, CounterComponent],
+  imports: [RouterOutlet],
 
   templateUrl: './app.html',
   styleUrl: './app.scss'
