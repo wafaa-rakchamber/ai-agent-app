93d794ca0b1ce4fa4a30224544cf6ee06bf85b6c
WAFAA <wafaa@rakchamber.ae>
2025-08-12 08:46:07 +0400

    working after wafaa app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TodoAppComponent } from './todo/todo-app.component';
-import { TodoListComponent } from './todo/todo-list.component';
-import { TodoItemComponent } from './todo/todo-item.component';
-import { AddTodoComponent } from './todo/add-todo.component';
+import { WafaaProject } from './wafaa-project/wafaa-project';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TodoAppComponent, TodoListComponent, TodoItemComponent, AddTodoComponent],
+  imports: [RouterOutlet, TodoAppComponent, WafaaProject],
   templateUrl: './app.html',
-  styleUrl: './app.scss',
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('ai-agent-app');
